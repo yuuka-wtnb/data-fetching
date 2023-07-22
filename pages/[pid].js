851e698eae294d4093f57c777fc495ff69fcfa2c
yuuka-wtnb <yuuka.wtnb.011@gmail.com>
2023-07-23 00:53:26 +0900

    105 fifth commit

--- pages/[pid].js
+++ pages/[pid].js
@@ -17,15 +17,21 @@ function ProductDetailPage(props) {
   );
 }
 
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  //Json.parseはJsonデータを通常のJavaScriptオブジェクトに変換
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
 
   const productId = params.pid;
 
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-  const jsonData = await fs.readFile(filePath);
-  //Json.parseはJsonデータを通常のJavaScriptオブジェクトに変換
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   const product = data.products.find((product) => product.id === productId);
 
@@ -37,9 +43,15 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [{ params: { pid: "p1" } }],
-    fallback: true,
+    paths: pathsWithParams,
+    fallback: false,
   };
 }
 
