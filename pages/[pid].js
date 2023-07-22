57680bf8615658411a1c638903f43d900c1a3418
yuuka-wtnb <yuuka.wtnb.011@gmail.com>
2023-07-23 00:01:54 +0900

    102 fourth commit

---
+++ pages/[pid].js
@@ -0,0 +1,46 @@
+import { Fragment } from "react";
+import path from "path";
+import fs from "fs/promises";
+
+function ProductDetailPage(props) {
+  const { loadedProduct } = props;
+
+  return (
+    <Fragment>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </Fragment>
+  );
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+
+  const productId = params.pid;
+
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  //Json.parseはJsonデータを通常のJavaScriptオブジェクトに変換
+  const data = JSON.parse(jsonData);
+
+  const product = data.products.find((product) => product.id === productId);
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { pid: "p1" } },
+      { params: { pid: "p2" } },
+      { params: { pid: "p3" } },
+    ],
+    fallback: false
+  };
+}
+
+export default ProductDetailPage;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
-import path from 'path';
-import fs from 'fs/promises';
+import path from "path";
+import fs from "fs/promises";
+import Link from "next/link";
 
 function HomePage(props) {
   const { products } = props;
 
   return (
     <ul>
-      {products.map((products) => (
-        <li key={products.id}>{products.title}</li>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
       ))}
     </ul>
   );
@@ -16,16 +19,28 @@ function HomePage(props) {
 export async function getStaticProps() {
   //cwd=current working directory
   //'data'-> because we wanna dive into the data folder
-  const filePath = path.join(process.cwd(), 'data','dummy-backend.json')
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   //Json.parseはJsonデータを通常のJavaScriptオブジェクトに変換
   const data = JSON.parse(jsonData);
 
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
   return {
     props: {
-      products:data.products
+      products: data.products,
     },
-    revalidate: 10
+    revalidate: 10,
   };
 }
 
